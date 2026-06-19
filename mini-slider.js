f54f555acb26105332e9aab128e75789d8dfa5a3
vichuprakova-crypto <vichuprakova@gmail.com>
2026-06-19 15:21:29 +0300

    Update mini-slider.js

--- mini-slider.js
+++ mini-slider.js
@@ -1,18 +1,6 @@
 function initGenerated(generated_object, slide_duration) {
-  var $ = window.jQuery;
-
-  if (!$) {
-    console.warn("jQuery is not loaded");
-    return;
-  }
-
-  var sliderId = "tistols-mini-slider";
-  var styleId = sliderId + "-style";
-
-  if (!document.getElementById(styleId)) {
-    var style = document.createElement("style");
-    style.id = styleId;
-    style.textContent = `
+    document.write(`<!--SL10 - Модификация для Тильды. Слайдер из миниатюр с прогресс-баром https://mod.tistols.com/mini-slider -->
+<style>
 .tistols-circle-svg {
   width: 130%;
   height: 130%;
@@ -26,7 +14,7 @@ function initGenerated(generated_object, slide_duration) {
   opacity: 1;
   stroke-dasharray: 251;
   stroke-dashoffset: 251;
-  animation: tistolsfillCircle ${slide_duration}s linear forwards;
+  animation: tistolsfillCircle ` + slide_duration + `s linear forwards;
 }
 
 .tistolsPicMS .tn-atom {
@@ -64,6 +52,8 @@ function initGenerated(generated_object, slide_duration) {
   transform: scale(0.8);
   transition: transform 0.1s linear;
   cursor: pointer;
+  -webkit-tap-highlight-color: transparent;
+  touch-action: manipulation;
 }
 
 [class*="tistolsCircleTab"]:hover {
@@ -89,172 +79,139 @@ function initGenerated(generated_object, slide_duration) {
     transform: none;
   }
 }
-`;
-    document.head.appendChild(style);
-  }
+</style>
 
-  var info = {};
-
-  try {
-    info = Function('"use strict"; return ({' + generated_object + '});')();
-  } catch (error) {
-    console.warn("Mini slider data error", error);
-    return;
-  }
-
-  var svg = `<svg class="tistols-circle-svg" viewBox="0 0 100 100" style="position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);">
+<script>
+var svg = \`<svg class="tistols-circle-svg" viewBox="0 0 100 100" style="position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);">
   <circle cx="50" cy="50" r="40" fill="none" stroke="transparent" stroke-width="2"></circle>
   <circle class="tistols-animated-circle" cx="50" cy="50" r="40" fill="none" stroke="#00f" stroke-width="2"></circle>
-</svg>`;
+</svg>\`;
 
-  function rgbToHex(rgb) {
-    if (!rgb) return "";
+function rgbToHex(rgb) {
+  if (!rgb) return "";
 
-    var rgbValues = rgb.match(/^rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)$/);
-    if (!rgbValues) return "";
+  var rgbValues = rgb.match(/^rgb\\(\\s*(\\d{1,3})\\s*,\\s*(\\d{1,3})\\s*,\\s*(\\d{1,3})\\s*\\)$/);
+  if (!rgbValues) return "";
 
-    function hex(x) {
-      return ("0" + parseInt(x, 10).toString(16)).slice(-2);
-    }
-
-    return "#" + hex(rgbValues[1]) + hex(rgbValues[2]) + hex(rgbValues[3]);
+  function hex(x) {
+    return ("0" + parseInt(x, 10).toString(16)).slice(-2);
   }
 
-  function getTabNumber($tab) {
-    var classes = ($tab.attr("class") || "").split(/\s+/);
+  return "#" + hex(rgbValues[1]) + hex(rgbValues[2]) + hex(rgbValues[3]);
+}
 
-    for (var i = 0; i < classes.length; i++) {
-      var match = classes[i].match(/^tistolsCircleTab(\d+)$/);
+function getTabNumber($tab) {
+  var className = $tab.attr("class") || "";
+  var match = className.match(/tistolsCircleTab(\\d+)/);
+  return match ? parseInt(match[1], 10) : null;
+}
 
-      if (match) {
-        return parseInt(match[1], 10);
-      }
+function startAnimation(tabNumber) {
+  var circle = jQuery(".tistolsCircleTab" + tabNumber + " .tistols-animated-circle");
+
+  circle.each(function () {
+    this.style.animation = "none";
+    this.offsetHeight;
+    this.style.animation = "";
+    this.style.strokeDashoffset = this.style.strokeDasharray;
+  });
+}
+
+function updateInfo(tabNumber) {
+  var info = {` + generated_object + `};
+
+  if (!info.hasOwnProperty(tabNumber)) return;
+
+  var data = info[tabNumber];
+
+  jQuery(".tistolsTitleMS .tn-atom").text(data.title || "");
+  jQuery(".tistolsPersonMS .tn-atom").text(data.person || "");
+  jQuery(".tistolsDescMS .tn-atom").text(data.decs || "");
+
+  var pic = jQuery(".tistolsPicMS .tn-atom");
+
+  if (pic.length && data.pic) {
+    var oldPic = pic.css("background-image");
+
+    pic.css("--tistols-prev-pic", oldPic);
+    pic.addClass("is-changing");
+
+    if (pic[0]) {
+      pic[0].offsetHeight;
     }
 
-    return null;
-  }
-
-  function startAnimation(tabNumber) {
-    var $circle = $(".tistolsCircleTab" + tabNumber + " .tistols-animated-circle");
-
-    $circle.each(function () {
-      this.style.animation = "none";
-      this.offsetHeight;
-      this.style.animation = "";
-      this.style.strokeDashoffset = this.style.strokeDasharray;
-    });
-  }
-
-  function updateInfo(tabNumber) {
-    if (!Object.prototype.hasOwnProperty.call(info, tabNumber)) return;
-
-    var data = info[tabNumber] || {};
-    var $title = $(".tistolsTitleMS .tn-atom");
-    var $person = $(".tistolsPersonMS .tn-atom");
-    var $desc = $(".tistolsDescMS .tn-atom");
-    var $pic = $(".tistolsPicMS .tn-atom");
-
-    $title.text(data.title || "");
-    $person.text(data.person || "");
-    $desc.text(data.decs || "");
-
-    if ($pic.length && data.pic) {
-      var oldPic = $pic.css("background-image");
-
-      $pic.css("--tistols-prev-pic", oldPic);
-      $pic.addClass("is-changing");
-
-      if ($pic[0]) {
-        $pic[0].offsetHeight;
-      }
-
-      $pic.css("background-image", "url(" + data.pic + ")");
-
-      requestAnimationFrame(function () {
-        $pic.removeClass("is-changing");
-      });
-    }
-
-    var $animatedText = $title.add($person).add($desc);
-
-    $animatedText.removeClass("fade");
+    pic.css("background-image", "url(" + data.pic + ")");
 
     requestAnimationFrame(function () {
-      $animatedText.addClass("fade");
+      pic.removeClass("is-changing");
     });
-
-    setTimeout(function () {
-      $animatedText.removeClass("fade");
-    }, 800);
   }
 
-  function initMiniSlider() {
-    var $tabs = $('[class*="tistolsCircleTab"]');
+  var animatedText = jQuery(".tistolsTitleMS .tn-atom, .tistolsPersonMS .tn-atom, .tistolsDescMS .tn-atom");
 
-    if (!$tabs.length) return;
+  animatedText.addClass("fade");
 
-    if ($tabs.data("tistolsMiniSliderReady")) return;
-    $tabs.data("tistolsMiniSliderReady", true);
+  setTimeout(function () {
+    animatedText.removeClass("fade");
+  }, 800);
+}
 
-    $tabs.each(function () {
-      var $tab = $(this);
-      var $atom = $tab.find(".tn-atom").first();
+jQuery(function () {
+  var tabs = jQuery('[class*="tistolsCircleTab"]');
+  var totalSlides = tabs.length;
 
-      if (!$atom.length) return;
+  if (!totalSlides) return;
 
-      if (!$atom.find(".tistols-circle-svg").length) {
-        $atom.append(svg);
-      }
+  tabs.each(function () {
+    var tab = jQuery(this);
+    var atom = tab.find(".tn-atom").first();
 
-      var $circle = $atom.find(".tistols-animated-circle");
-      var hexColor = rgbToHex($atom.css("background-color"));
+    if (!atom.length) return;
 
-      if (hexColor) {
-        $circle.attr("stroke", hexColor);
-      }
-    });
+    if (!atom.find(".tistols-circle-svg").length) {
+      atom.append(svg);
+    }
 
-    $tabs.removeClass("act");
-    $(".tistolsCircleTab1").addClass("act");
+    var circle = atom.find(".tistols-animated-circle");
+    var rgbColor = atom.css("background-color");
+    var hexColor = rgbToHex(rgbColor);
 
-    updateInfo(1);
-    startAnimation(1);
-
-    $tabs.off("click.tistolsMiniSlider").on("click.tistolsMiniSlider", function () {
-      var tabNumber = getTabNumber($(this));
-
-      if (!tabNumber) return;
-
-      $tabs.removeClass("act");
-      $(this).addClass("act");
-
-      updateInfo(tabNumber);
-      startAnimation(tabNumber);
-    });
-
-    $(".tistols-animated-circle")
-      .off("animationend.tistolsMiniSlider")
-      .on("animationend.tistolsMiniSlider", function () {
-        var $activeTab = $tabs.filter(".act").first();
-        var currentTab = getTabNumber($activeTab) || 1;
-        var totalSlides = $tabs.length;
-        var nextTab = (currentTab % totalSlides) + 1;
-
-        $tabs.removeClass("act");
-        $(".tistolsCircleTab" + nextTab).addClass("act");
-
-        updateInfo(nextTab);
-        startAnimation(nextTab);
-      });
-  }
-
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", initMiniSlider);
-  } else {
-    initMiniSlider();
-  }
-
-  window.addEventListener("load", function () {
-    setTimeout(initMiniSlider, 300);
+    if (hexColor) {
+      circle.attr("stroke", hexColor);
+    }
   });
+
+  tabs.removeClass("act");
+  jQuery(".tistolsCircleTab1").addClass("act");
+
+  updateInfo(1);
+  startAnimation(1);
+
+  tabs.off("click.tistolsMiniSlider touchend.tistolsMiniSlider").on("click.tistolsMiniSlider touchend.tistolsMiniSlider", function (event) {
+    event.preventDefault();
+
+    var tab = jQuery(this);
+    var tabNumber = getTabNumber(tab);
+
+    if (!tabNumber) return;
+
+    tabs.removeClass("act");
+    tab.addClass("act");
+
+    updateInfo(tabNumber);
+    startAnimation(tabNumber);
+  });
+
+  jQuery(".tistols-animated-circle").off("animationend.tistolsMiniSlider").on("animationend.tistolsMiniSlider", function () {
+    var currentTab = getTabNumber(jQuery(".act").first()) || 1;
+    var nextTab = (currentTab % totalSlides) + 1;
+
+    tabs.removeClass("act");
+    jQuery(".tistolsCircleTab" + nextTab).addClass("act");
+
+    startAnimation(nextTab);
+    updateInfo(nextTab);
+  });
+});
+<\/script>`);
 }
